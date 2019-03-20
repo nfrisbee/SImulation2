1df7569a4d01be840ef6cd8652a2604d84d3fe75
nfrisbee <nicolefrisbee@yahoo.com>
2019-03-20 15:51:30 -0600

    deleted a line

--- houser/Server/index.js
+++ houser/Server/index.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
+const controller = require('./controller.js');
+const massive = require('massive');
+require('dotenv').config({path: '../.env'}); 
 
 
+//Massive
+let {
+    DATABASE_CONNECTION
+} = process.env;
+
+massive(DATABASE_CONNECTION).then(dbInstance => {
+    app.set('db', dbInstance);
+    console.log('Connected!')
+}).catch(error => {
+    console.log(DATABASE_CONNECTION)
+    console.log('Db not connected:', error);
+});
+
+
+//endpoints
 app.get('/', function (req, res) {
     res.send('Hello there! I am a cute lil server!');
 });
